79ab6def7d9e2d09927c0071b6a64bf02617bc8e
mark <markm@cs.wisc.edu>
2020-10-09 15:10:58 -0500

    add sysfs for mm_econ enable

--- mm/estimator.c
+++ mm/estimator.c
@@ -1,6 +1,85 @@
+/*
+ * Implementation of cost-benefit based memory management.
+ */
 
 #include <linux/printk.h>
 #include <linux/mm_econ.h>
+#include <linux/mm.h>
+#include <linux/kobject.h>
+#include <linux/init.h>
+
+#define HUGE_PAGE_ORDER 9
+
+///////////////////////////////////////////////////////////////////////////////
+// sysfs files
+
+// Modes:
+// - 0: off (just use default linux behavior)
+// - 1: on (cost-benefit estimation)
+static int mm_econ_mode = 0;
+
+static ssize_t enabled_show(struct kobject *kobj,
+        struct kobj_attribute *attr, char *buf)
+{
+    return sprintf(buf, "%d\n", mm_econ_mode);
+}
+
+static ssize_t enabled_store(struct kobject *kobj,
+        struct kobj_attribute *attr,
+        const char *buf, size_t count)
+{
+    int mode;
+    int ret;
+
+    ret = kstrtoint(buf, 0, &mode);
+
+    if (ret != 0) {
+        mm_econ_mode = 0;
+        return ret;
+    }
+    else if (mode >= 0 && mode <= 1) {
+        mm_econ_mode = mode;
+        return count;
+    }
+    else {
+        mm_econ_mode = 0;
+        return -EINVAL;
+    }
+}
+static struct kobj_attribute enabled_attr =
+__ATTR(enabled, 0644, enabled_show, enabled_store);
+
+static struct attribute *mm_econ_attr[] = {
+    &enabled_attr.attr,
+    NULL,
+};
+
+static const struct attribute_group mm_econ_attr_group = {
+    .attrs = mm_econ_attr,
+};
+
+static int __init mm_econ_init(void)
+{
+    struct kobject *mm_econ_kobj;
+    int err;
+
+    mm_econ_kobj = kobject_create_and_add("mm_econ", mm_kobj);
+    if (unlikely(!mm_econ_kobj)) {
+        pr_err("failed to create mm_econ kobject\n");
+        return -ENOMEM;
+    }
+
+    err = sysfs_create_group(mm_econ_kobj, &mm_econ_attr_group);
+    if (err) {
+        pr_err("failed to register mm_econ group\n");
+        kobject_put(mm_econ_kobj);
+        return err;
+    }
+
+    return 0;
+}
+subsys_initcall(mm_econ_init);
+
 
 // Estimates the change in the given metrics under the given action. Updates
 // the given cost struct in place.
@@ -48,6 +127,10 @@ mm_estimate_changes(const struct mm_action *action, struct mm_cost_delta *cost)
 // action associated with `cost` should be TAKEN, and false otherwise.
 bool mm_decide(const struct mm_cost_delta *cost)
 {
-    // TODO(markm): for now default to the normal linux behavior
-    return true;
+    if (mm_econ_mode == 0) {
+        return true;
+    } else if (mm_econ_mode == 1) {
+        // TODO(markm): for now default to the normal linux behavior
+        return true;
+    }
 }
